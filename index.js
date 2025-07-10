a2405287b2b8c62bd36a40e7b08079d5a7f6c7aa
arpan-gt <arpan.main@outlook.com>
2025-07-10 18:13:39 +0530

    database connection called and todo route created

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { userRouter } from "./routes/user.routes.js";
 import dotenv from "dotenv";
+import { connectDB } from "./utils/db.js";
+import { taskRouter } from "./routes/task.routes.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.post("/add", (req, res) => {
   res.send("add new task");
@@ -17,6 +21,7 @@ app.get("/mytasks", (req, res) => {
 });
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/todo", taskRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
