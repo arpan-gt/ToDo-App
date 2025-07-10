bf5fdb6479936e2dcdffb50598648f8de7f09aa3
arpan-gt <arpan.main@outlook.com>
2025-07-10 17:07:22 +0530

    auth middleware created

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+ const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!process.env.JWT_SECRET) {
+    return res
+      .status(500)
+      .json({ message: "Server error: Missing JWT secret" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+export {authenticate}
