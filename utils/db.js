55246abc5a4422f42f598f9f920053c999321d72
arpan-gt <arpan.main@outlook.com>
2025-07-10 17:05:45 +0530

    database setup

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = async (req, res) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected to DB");
+  } catch (err) {
+    console.log("mongodb connection failed", err.message);
+    process.exit(1);
+  }
+};
+export { connectDB };
