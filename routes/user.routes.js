2ab2ba063ad4f5a8b304a1f1744ca22c27980d15
arpan-gt <arpan.main@outlook.com>
2025-07-10 18:11:17 +0530

    user signin route added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
-const userRouter = express.Router();
 import jwt from "jsonwebtoken";
 import User from "../models/User.models.js";
+import bcrypt from "bcrypt";
+const userRouter = express.Router();
 
 userRouter.post("/signup", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res.json({
+    return res.json({
       message: "email and password are required",
     });
   }
@@ -21,7 +22,7 @@ userRouter.post("/signup", async (req, res) => {
     }
 
     const user = await User.create({ email, password });
-    res.json({
+    return res.json({
       message: "user signedup successfully",
     });
   } catch (e) {
@@ -32,7 +33,39 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/signin", async (req, res) => {});
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({
+      message: "All fields are required",
+    });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({
+      message: "email or password incorrect",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "Email or password incorrect",
+    });
+  }
+
+  const token = jwt.sign({ userId: user._id }, "SECRET", { expiresIn: "1h" });
+
+  return res.json({
+    message: "user signedIn",
+    user: {
+      userId: user._id,
+      email: email,
+      token,
+    },
+  });
+});
 
 userRouter.post("/", async (req, res) => {});
 export { userRouter };
