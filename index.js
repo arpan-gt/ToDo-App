1433ce7088ba67728990c1a1b732b72d99c31d27
arpan-gt <arpan.main@outlook.com>
2025-07-10 15:39:58 +0530

    configured dotenv

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from "express";
+import { userRouter } from "./routes/user.routes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/add", (req, res) => {
   res.send("add new task");
 });
 
 app.get("/mytasks", (req, res) => {
-  
-})
+  res.send("my all tasks");
+});
+
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
