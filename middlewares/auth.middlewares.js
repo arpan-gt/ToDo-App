d5b3eb9b4abdedb6c6638fd19177eb937b43047c
arpan-gt <arpan.main@outlook.com>
2025-07-12 17:19:20 +0530

    updated auth middleware code

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
-const authenticate = (req, res, next) => {
+export const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
+
+  console.log("AUTH HEADER:", authHeader);
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
@@ -9,18 +11,17 @@ const authenticate = (req, res, next) => {
 
   const token = authHeader.split(" ")[1];
 
-  if (!process.env.JWT_SECRET) {
-    return res
-      .status(500)
-      .json({ message: "Server error: Missing JWT secret" });
-  }
-
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "SECRET");
+
+    console.log("DECODED JWT:", decoded);
+
+    req.user = { userId: decoded.userId || decoded.id };
+
+    console.log("REQ.USER:", req.user);
+
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
-export { authenticate };
