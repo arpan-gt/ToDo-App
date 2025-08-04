ce5de6a3fd7d708aa9cd9debc45806f53a3220ed
arpan-gt <arpan.main@outlook.com>
2025-08-04 21:15:46 +0530

    added middleware to catch undeined routes

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ import { userRouter } from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./utils/db.js";
 import { taskRouter } from "./routes/task.routes.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 connectDB();
-
-app.post("/add", (req, res) => {
-  res.send("add new task");
-});
-
-app.get("/mytasks", (req, res) => {
-  res.send("my all tasks");
-});
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/todo", taskRouter);
 
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
 });
